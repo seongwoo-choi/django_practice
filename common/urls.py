438af098a8f19c27e3705dc0a0b31c27e3d16dbf
seongwoo_choi <how0326@gmail.com>
2021-09-14 23:10:43 +0900

    로그인 구현
    - login.html, form_errors.html 생성
    - django.contrib.auth 앱의 LoginView 클래스를 활용
    - navbar.html 에 로그인 url 추가
    - settings.py INSTALLED_APPS 에 common.apps.CommonConfig 추가
    - LOGIN_REDIRECT_URL = '/' 추가 => 로그인 시 루트로 리다이렉트 되도록 설정
    - config/url.py 에 루트 경로 추가

---
+++ common/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+app_name = 'common'
+
+urlpatterns = [
+    # django.contrib.auth 앱의 LoginView 클래스를 활용 했으므로 별도의 views.py 파일 수정이 필요 없다!
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login')
+]
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,7 @@ from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
 ]
