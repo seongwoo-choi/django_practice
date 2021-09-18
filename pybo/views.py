30bb015bf1342f9820eabfef4a92ff5c4a66e25d
seongwoo_choi <how0326@gmail.com>
2021-09-18 23:02:05 +0900

    질문 삭제 기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -156,3 +156,16 @@ def question_modify(request, question_id):
     # context 에 빈 form 이 실려서 pybo/question_form.html 로 렌더가 된다.
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    pybo 질문삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
