ecc727f9214aec3c60b27638d4e0a79ca59a766e
seongwoo_choi <how0326@gmail.com>
2021-09-20 09:23:14 +0900

    질문 추천 기능 추가
    - 다대다 관계를 사용하여 Question, Answer 모델에 voter 속성 추가

--- pybo/models.py
+++ pybo/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     modify_date = models.DateTimeField(null=True, blank=True)
     content = models.TextField()
     create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
@@ -18,6 +19,7 @@ class Answer(models.Model):
     modify_date = models.DateTimeField(null=True, blank=True)
     content = models.TextField()
     create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 
 class Comment(models.Model):
