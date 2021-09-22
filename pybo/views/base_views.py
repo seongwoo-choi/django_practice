d4804cd3ab7dc462a7fda6206931186eb0cceea8
seongwoo_choi <how0326@gmail.com>
2021-09-22 21:01:31 +0900

    검색 기능 추가 & 최신순/인기순/날짜순 으로 검색가능 하도록 설정

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,28 +1,8 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
-import logging
-
-# 로그 생성
-logger = logging.getLogger()
-
-# 로그의 출력 기준 설정
-logger.setLevel(logging.INFO)
-
-# log 출력 형식
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# log 출력
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
-
-# log를 파일에 출력
-file_handler = logging.FileHandler('my.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
 
 # Create your views here.
 
@@ -36,18 +16,41 @@ def index(request):
     # localhost:8000/pybo/?page=1, page=1 인 값을 page 에 저장
     # 페이지 초기값을 설정해주는 것
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
+    so = request.GET.get('so', 'recent')
+
+
+    # annotate 함수를 사용하면 임시로 필드를 추가해 줄 수 있다.
+    # 그래서 num_voter 라는 필드를 속성으로 추가해 준 것이다.
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(
+            num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:
+        question_list = Question.objects.order_by('-create_date')
+
 
     # Question 데이터들을 create_date 열의 내림차순으로 정렬하여 조회한다.
-    question_list = Question.objects.order_by('-create_date')
+    # question_list = Question.objects.order_by('-create_date')
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
+
 
     # 페이징 처리
     # 페이지당 10개 씩 보여주기
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
 
-    logger.info(page)
     return render(request, 'pybo/question_list.html', context)
 
 
